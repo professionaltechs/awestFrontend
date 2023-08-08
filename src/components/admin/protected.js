7f28f0e5a6840e84e2675a75a2813aac4a3324eb
professionaltechs <115035072+professionaltechs@users.noreply.github.com>
2023-08-08 15:48:21 -0700

    changes

--- src/components/admin/protected.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const AdminProtected = ({ children }) => {
-    if (localStorage.getItem('isLoggedIn') === "true") {
-        return children;
-    }
-    return <Navigate to="/admin/login" replace/>;
-};
